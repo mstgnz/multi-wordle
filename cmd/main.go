c8ddfac537860485f39e31f44e2d114ac13c3694
mstgnz <mesutgenez@hotmail.com>
2024-01-02 22:49:33 +0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"golang.org/x/net/websocket"
+)
+
+func main() {
+	port := "3000"
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	socket := NewSocket()
+	http.Handle("/ws", websocket.Handler(socket.Handler))
+
+	done := make(chan bool)
+	go func() {
+		panic(http.ListenAndServe(":"+port, nil))
+	}()
+	log.Println("Started Websocket on :" + port)
+	<-done
 }
